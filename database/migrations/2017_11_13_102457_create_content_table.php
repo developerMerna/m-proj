3a065cf3ee65e56c29ec0e33149ce1bfa7853fd3
developerM <flowerm37@yahoo.com>
2017-11-13 12:39:26 +0200

    content table

---
+++ database/migrations/2017_11_13_102457_create_content_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user-id');
+            $table->string('image');
+            $table->string('img-link');
+            $table->string('img-text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content');
+    }
+}
